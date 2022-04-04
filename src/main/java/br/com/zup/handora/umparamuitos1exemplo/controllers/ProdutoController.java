534d5bcfaab0f9926b3dc5923d26251e16858e98
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-04 19:13:54 -0300

    Add Produto creation method to controller

--- src/main/java/br/com/zup/handora/umparamuitos1exemplo/controllers/ProdutoController.java
+++ src/main/java/br/com/zup/handora/umparamuitos1exemplo/controllers/ProdutoController.java
@@ -1,8 +1,19 @@
 package br.com.zup.handora.umparamuitos1exemplo.controllers;
 
+import java.net.URI;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.zup.handora.umparamuitos1exemplo.models.Produto;
+import br.com.zup.handora.umparamuitos1exemplo.models.ProdutoDTO;
 import br.com.zup.handora.umparamuitos1exemplo.repositories.ProdutoRepository;
 
 @RestController
@@ -15,6 +26,19 @@ public class ProdutoController {
 
     public ProdutoController(ProdutoRepository produtoRepository) {
         this.produtoRepository = produtoRepository;
+    }
+
+    @Transactional
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody @Valid ProdutoDTO produtoDTO,
+                                       UriComponentsBuilder uriComponentsBuilder) {
+        Produto produto = produtoRepository.save(produtoDTO.toModel());
+
+        URI location = uriComponentsBuilder.path(ProdutoController.BASE_URI + "/{id}")
+                                           .buildAndExpand(produto.getId())
+                                           .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }
