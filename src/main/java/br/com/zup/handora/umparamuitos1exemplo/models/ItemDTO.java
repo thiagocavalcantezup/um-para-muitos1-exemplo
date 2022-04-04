a395ff35ea862917983ad919c02f990cf44253ba
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-04 18:25:02 -0300

    Add Item DTO class

---
+++ src/main/java/br/com/zup/handora/umparamuitos1exemplo/models/ItemDTO.java
@@ -0,0 +1,29 @@
+package br.com.zup.handora.umparamuitos1exemplo.models;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+public class ItemDTO {
+
+    @NotNull
+    @Positive
+    private Long produtoId;
+
+    @NotNull
+    @Positive
+    private Integer quantidade;
+
+    public ItemDTO(@NotNull @Positive Long produtoId, @NotNull @Positive Integer quantidade) {
+        this.produtoId = produtoId;
+        this.quantidade = quantidade;
+    }
+
+    public Long getProdutoId() {
+        return produtoId;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+}
