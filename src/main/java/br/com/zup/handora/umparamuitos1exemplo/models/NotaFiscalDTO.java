3d934da944f1cf4680304adcb6f89e9099fdf475
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-04 19:36:52 -0300

    Add @Valid annotation to validate nested items

--- src/main/java/br/com/zup/handora/umparamuitos1exemplo/models/NotaFiscalDTO.java
+++ src/main/java/br/com/zup/handora/umparamuitos1exemplo/models/NotaFiscalDTO.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -18,6 +19,7 @@ public class NotaFiscalDTO {
     @PositiveOrZero
     private BigDecimal total;
 
+    @Valid
     @NotEmpty
     private Set<ItemDTO> itens;
 
