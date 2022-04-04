83bfa5617bb7a205306614e92ca41bd7d47db5e6
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-04 18:53:16 -0300

    Add NotaFiscal repository dependency to controller

--- src/main/java/br/com/zup/handora/umparamuitos1exemplo/controllers/NotaFiscalController.java
+++ src/main/java/br/com/zup/handora/umparamuitos1exemplo/controllers/NotaFiscalController.java
@@ -3,10 +3,18 @@ package br.com.zup.handora.umparamuitos1exemplo.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.zup.handora.umparamuitos1exemplo.repositories.NotaFiscalRepository;
+
 @RestController
 @RequestMapping(NotaFiscalController.BASE_URI)
 public class NotaFiscalController {
 
     public final static String BASE_URI = "/notas-fiscais";
+
+    private final NotaFiscalRepository notaFiscalRepository;
+
+    public NotaFiscalController(NotaFiscalRepository notaFiscalRepository) {
+        this.notaFiscalRepository = notaFiscalRepository;
+    }
 
 }
