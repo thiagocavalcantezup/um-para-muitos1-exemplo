599a41fa2362a77c7c76cdd285f617f586854a35
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-05 14:40:31 -0300

    Add contato creation method to controller

--- src/main/java/br/com/zup/handora/umparamuitos1exemplo/controllers/ContatoController.java
+++ src/main/java/br/com/zup/handora/umparamuitos1exemplo/controllers/ContatoController.java
@@ -1,8 +1,19 @@
 package br.com.zup.handora.umparamuitos1exemplo.controllers;
 
+import java.net.URI;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.zup.handora.umparamuitos1exemplo.models.Contato;
+import br.com.zup.handora.umparamuitos1exemplo.models.ContatoDTO;
 import br.com.zup.handora.umparamuitos1exemplo.repositories.ContatoRepository;
 
 @RestController
@@ -15,6 +26,20 @@ public class ContatoController {
 
     public ContatoController(ContatoRepository contatoRepository) {
         this.contatoRepository = contatoRepository;
+    }
+
+    @Transactional
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody @Valid ContatoDTO contatoDTO,
+                                       UriComponentsBuilder uriComponentsBuilder) {
+
+        Contato contato = contatoRepository.save(contatoDTO.toModel());
+
+        URI location = uriComponentsBuilder.path(BASE_URI + "/{id}")
+                                           .buildAndExpand(contato.getId())
+                                           .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }
